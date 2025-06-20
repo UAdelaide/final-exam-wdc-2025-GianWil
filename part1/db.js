95a25073f8c18b936f00b68596dc7b37e42082d8
GianWil <giangalaxy8@gmail.com>
2025-06-20 05:33:49 +0000

    [automatic commit] 2025-06-20 05:33:49 UTC 129.127.202.125

--- part1/db.js
+++ part1/db.js
@@ -3,11 +3,9 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '', // add your password if needed
-  database: 'DogWalkService',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  password: '',
+  database: 'DogWalkService'
 });
 
 module.exports = pool;
+
